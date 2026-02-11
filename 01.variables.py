f81158a6c278694ab70e6b432e945146fe0f0238
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2026-02-11 11:22:58 +0530

    New files has been added in PYTHON-SESSTIONS repo

--- 01.variables.py
+++ 01.variables.py
@@ -8,18 +8,34 @@ print("hello world") #this line of code prints hello world
 
 """
 
-#integer
-a = 1
+#   #integer
+#  a = 1
+#    print(a)
+#    print(type(a))
+#
+#    #Float
+#    b = 2.5
+#    print(b)
+#    print(type(b))
+
+#    # Complex
+#    c = 2 + 3j
+#    print(c)
+#    print(type(c))
+
+
+# Boolean datatype
+a = True # true is not 
 print(a)
+
+b = False
+print(b)
+
+
+# string datatype
+a = '123'
 print(type(a))
 
-#Float
-b = 2.5
+b = 'Hello World'
 print(b)
 print(type(b))
-
-# Complex
-c = 2 + 3j
-print(c)
-print(type(c))
-
