e829ee22c9d7460703cf9acec97f2b4532b15f4a
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2026-02-11 10:42:39 +0530

    New files has been added in PYTHON-SESSTIONS repo

---
+++ 01.variables.py
@@ -0,0 +1,25 @@
+
+"""
+var = 123
+print(var)
+#print(Var)
+
+print("hello world") #this line of code prints hello world
+
+"""
+
+#integer
+a = 1
+print(a)
+print(type(a))
+
+#Float
+b = 2.5
+print(b)
+print(type(b))
+
+# Complex
+c = 2 + 3j
+print(c)
+print(type(c))
+
